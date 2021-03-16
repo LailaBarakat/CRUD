54d3434cfc84b881ecc58d22452fbf62ec7490ff
Peter <dhondt.peter@gmail.com>
2021-03-16 14:22:39 +0100

    created StudenModel class

--- Model/StudentModel.php
+++ Model/StudentModel.php
@@ -3,10 +3,38 @@
 
 class StudentModel extends DataBase
 {
-    public function getStudents(){
-        $sql = "SELECT * FROM student";
-        $stmt = $this->connect()->prepare($sql);
-        $stmt->execute();
-        return $stmt->fetchAll();
+    private int $studentID;
+    private string $firstname;
+    private string $lastname;
+    private string $email;
+
+    public function __construct($id, $firstname, $lastname, $email)
+    {
+        $this->studentID = $id;
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+        $this->email = $email;
+
     }
+
+    public function getStudentID(): int
+    {
+        return $this->studentID;
+    }
+
+    public function getFirstname(): string
+    {
+        return $this->firstname;
+    }
+
+    public function getLastname(): string
+    {
+        return $this->lastname;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
 }
