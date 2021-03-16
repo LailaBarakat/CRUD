99c1b3283f07ba13a7b02e53faa46709b1d0763b
serkan <s.safran84@gmail.com>
2021-03-16 16:43:18 +0100

    form removed

--- View/studentOverview.php
+++ View/studentOverview.php
@@ -29,15 +29,15 @@ require "includes/header.php"
     <tbody>
     <?php foreach($students AS $student):?>
         <tr>
-            <td><?php echo htmlspecialchars($student['first_name'])?></td>
-            <td><?php echo htmlspecialchars($student['last_name'])?></td>
-            <td><?php echo htmlspecialchars($student['email'])?></td>
+            <td><?php echo htmlspecialchars($student->getfirst_name())?></td>
+            <td><?php echo htmlspecialchars($student->getlast_name())?></td>
+            <td><?php echo htmlspecialchars($student->getemail())?></td>
             <td>
-                <a href="?id=<?php echo $student['id']?>" class="btn btn-primary">Update</a>
+                <a href="?id=<?php echo $student->getid()?>" class="btn btn-primary">Update</a>
             </td>
             <td>
                 <form method="post">
-                    <input type="hidden" name="id" value="<?php echo $student['id']?>" />
+                    <input type="hidden" name="id" value="<?php echo $student->getid()?>" />
                     <input type="submit" name="delete" value="Delete" class="btn btn-danger">
                 </form>
             </td>
@@ -49,26 +49,6 @@ require "includes/header.php"
     <?php endforeach;?>
     </tbody>
 </table>
-
-<?php $saveLabel = "Save Record" ?>
-<!--Creating new Student-->
-<form method="post" id="create-student">
-    <h2><?php echo $saveLabel?></h2>
-
-    <input type="hidden" name="id" value="<?php echo $selectedStudent['id']?>" />
-
-    <label for="firstname">First name:</label>
-    <input type="text" name="firstname" id="firstname" required value="<?php echo htmlspecialchars($selectedStudent['firstname']) ?>"/>
-
-    <label for="lastname">Last name:</label>
-    <input type="text" name="lastname" id="lastname" required value="<?php echo htmlspecialchars($selectedStudent['lastname']) ?>"/>
-
-    <label for="lastname">Email:</label>
-    <input type="email" name="email" id="email" required value="<?php echo htmlspecialchars($selectedStudent['email']) ?>"/>
-
-    <input type="submit" value="<?php echo $saveLabel?>" />
-</form>
-
 
 <style>
     label {
