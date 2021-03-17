6d86adf585260537b770e3ad6f2036fadd563bf4
serkan <s.safran84@gmail.com>
2021-03-17 17:14:13 +0100

    x

--- View/studentCreate.php
+++ View/studentCreate.php
@@ -8,18 +8,43 @@ error_reporting(E_ALL);
 require 'includes/header.php'?>
     <!-- this is the view, try to put only simple if's and loops here.
     Anything complex should be calculated in the model -->
-    <section>
-        <h4>Create New</h4>
 
-        <p><a href="index.php?page=<?php echo $handler?>&run=create">To info page</a></p>
-        <p><a href="index.php?page=<?php echo $handler?>&run=delete">To info page</a></p>
-        <p><a href="index.php?page=<?php echo $handler?>&run=edit">To info page</a></p>
-        <p><a href="index.php?page=<?php echo $handler?>&run=general">To info page</a></p>
+        <h4>Create New Student</h4>
+    <form method="post" id="create-student">
+        <input type="hidden" name="id" value="">
+        <div>
+        <label for="first_name">First name:</label>
+        <input type="text" name="first_name" id="first_name" required />
+        </div>
 
-        <p><a href="index.php?page=teacher">Teachers</a></p>
-        <p><a href="index.php?page=class">Classes</a></p>
-        <p><a href="index.php?page=student">Students</a></p>
+        <div>
+        <label for="last_name">Last name:</label>
+        <input type="text" name="last_name" id="last_name"  required />
+        </div>
 
-        <p>Put your content here.</p>
-    </section>
+        <div>
+        <label for="email">Email:</label>
+        <input type="text" name="email" id="email"/>
+        </div>
+
+        <div >
+        <label for="class">Class:</label>
+        <input type="text" name="class" id="class" />
+        </div>
+        <input type="submit" value="Crate Student" class="btn btn-primary mt-4 ml-4"/>
+    </form>
+
+
+
+    <style>
+        label {
+            cursor: pointer;
+        }
+        label {
+            display: block;
+        }
+        form#create-student {
+            margin-left: 10px;
+        }
+    </style>
 <?php require 'includes/footer.php'?>
