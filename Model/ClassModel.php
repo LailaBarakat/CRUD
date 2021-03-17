629a4f5f5f2f590a5a20cfd860bf47d588437fd1
Peter <dhondt.peter@gmail.com>
2021-03-17 10:34:45 +0100

    created class ClassModel

--- Model/ClassModel.php
+++ Model/ClassModel.php
@@ -1,7 +1,38 @@
 <?php
-
+declare(strict_types=1);
 
 class ClassModel extends DataBase
 {
+    private int $classID;
+    private string $className;
+    private string $classLocation;
+    private string $teacherID;
 
+    public function __construct($id, $className, $classLocation, $teacherID)
+    {
+        $this->classID = $id;
+        $this->className = $className;
+        $this->classLocation = $classLocation;
+        $this->teacherID = $teacherID;
+    }
+
+    public function getclassid(): int
+    {
+        return $this->classID;
+    }
+
+    public function getclassname(): string
+    {
+        return $this->className;
+    }
+
+    public function getclasslocation(): string
+    {
+        return $this->classLocation;
+    }
+
+    public function getteacherid(): string
+    {
+        return $this->teacherID;
+    }
 }
