d1691fe0f84817dbc28452051b88ad51227179af
Thomas Pieters <pieters.tg@gmail.com>
2021-03-17 10:18:26 +0100

    added delete/fetch-one/update methods

--- Model/StudentLoader.php
+++ Model/StudentLoader.php
@@ -3,58 +3,123 @@
 require_once 'StudentModel.php';
 require_once 'DataBase.php';
 
-class StudentLoader{
+class StudentLoader
+{
 
-private array $studentArray = [];
+    public function getStudent(int $id): StudentModel
+    {
 
+        try {
 
-    public function getAllStudents ():array {
+            $DB = new DataBase();
+            $conn = $DB->connect();
 
-    try {
-        $DB = new DataBase();
-        $conn = $DB->connect();
+            $stmt = $conn->query("SELECT id, first_name, last_name, email, classID FROM Student WHERE id = $id");
+            $result = $stmt->fetch();
+            $student = new StudentModel($result['id'], $result['first_name'], $result['last_name'], $result['email'], $result['classID']);
 
-        $stmt = $conn->prepare("SELECT id, first_name, last_name, email, classID FROM Student");
-        $stmt->execute();
-
-        // set the resulting array to associative
-        $results=$stmt->fetchAll(PDO::FETCH_ASSOC);
-        foreach ($results as $row) {
-            $student=new StudentModel($row['id'],$row['first_name'],$row['last_name'],$row['email'],$row['classID']);
-            array_push($this->studentArray, $student);
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
         }
-    } catch(PDOException $e) {
-        echo "Error: " . $e->getMessage();
+        $conn = null;
+        return $student;
+
     }
-    $conn = null;
+
+    private array $studentArray = [];
+
+    public function getAllStudents(): array
+    {
+
+        try {
+            $DB = new DataBase();
+            $conn = $DB->connect();
+
+            $stmt = $conn->prepare("SELECT id, first_name, last_name, email, classID FROM Student");
+            $stmt->execute();
+
+            // set the resulting array to associative
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($results as $row) {
+                $student = new StudentModel($row['id'], $row['first_name'], $row['last_name'], $row['email'], $row['classID']);
+                array_push($this->studentArray, $student);
+            }
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+
+        return $this->studentArray;
+
+    }
+
+    public function insertNewStudent(StudentModel $student): void
+    {
+
+        try {
+            $DB = new DataBase();
+            $conn = $DB->connect();
 
 
-    return $this->studentArray;
+            $id = $conn->lastInsertId();
+            $first_name = $student->getfirst_name();
+            $last_name = $student->getlast_name();
+            $email = $student->getemail();
+            $classID = $student->getclassID();
 
-}
-
-    public function insertNewStudent (StudentModel $student):void {
-
-    try {
-        $DB = new DataBase();
-        $conn = $DB->connect();
-
-        $id=$student->getid();
-        $first_name=$student->getfirst_name();
-        $last_name=$student->getlast_name();
-        $email=$student->getemail();
-        $classID=$student->getclassID();
-
-        $sql = "INSERT INTO Student (id, first_name, last_name, email, classID)
+            $sql = "INSERT INTO Student (id, first_name, last_name, email, classID)
                 VALUES ('$id', '$first_name', '$last_name', '$email', '$classID')";
-        // use exec() because no results are returned
-        $conn->exec($sql);
-        //echo "New record created successfully";
-    } catch(PDOException $e) {
-        //echo $sql . "<br>" . $e->getMessage();
+            // use exec() because no results are returned
+            $conn->exec($sql);
+            //echo "New record created successfully";
+        } catch (PDOException $e) {
+            //echo $sql . "<br>" . $e->getMessage();
+        }
+
+        $conn = null;
     }
 
-    $conn = null;
-}
+    public function updateStudent(StudentModel $student): void
+    {
+        try {
+            $DB = new DataBase();
+            $conn = $DB->connect();
+
+            $id = $student->getid();
+            $first_name = $student->getfirst_name();
+            $last_name = $student->getlast_name();
+            $email = $student->getemail();
+            $classID = $student->getclassID();
+            $sql = "UPDATE Student SET  first_name = '$first_name', last_name = '$last_name', email = '$email', classID = '$classID' WHERE id = '$id'";
+            $conn->exec($sql);
+
+        } catch (PDOException $e) {
+            //echo $sql . "<br>" . $e->getMessage();
+        }
+        $conn = null;
+    }
+
+    public function deleteStudent(StudentModel $student): void
+    {
+        try {
+            $DB = new DataBase();
+            $conn = $DB->connect();
+
+            $id = $student->getid();
+            $sql = "DELETE FROM Student WHERE id ='$id'";
+            $conn->exec($sql);
+
+        } catch (PDOException $e) {
+            //echo $sql . "<br>" . $e->getMessage();
+        }
+        $conn = null;
+
+
+
+
+    }
+
+
+
 
 }
