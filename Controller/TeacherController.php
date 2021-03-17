dbc24e1c7fff7f746fbc691d6c58abe1ee6f8a33
Laila Barakat <laila.n.barakat@gmail.com>
2021-03-17 16:28:29 +0100

    creating teachercontroller

--- Controller/TeacherController.php
+++ Controller/TeacherController.php
@@ -1,18 +1,57 @@
 <?php
 
 
-class TeacherController
-{
-//render function with both $_GET and $_POST vars available if it would be needed.
-    public function render(array $GET, array $POST)
+class TeacherController {
+
+    //render function with both $_GET and $_POST vars available if it would be needed.
+    public function render(array $GET, array $POST): void
     {
-        //this is just example code, you can remove the line below
-        $user = new User('John Smith');
 
-        //you should not echo anything inside your controller - only assign vars here
-        // then the view will actually display them.
+        $pdo = new TeacherLoader();
 
-        //load the view
-        require 'View/homepage.php';
+        if(!empty($_POST['first_name']) && !empty($_POST['last_name'])){
+            if(empty($_POST['id'])){
+                $new = new TeacherModel( null, $_POST['first_name'], $_POST['last_name'], $_POST['email']);
+                $pdo->insertNewTeacher($new);
+
+                $message= 'New Teacher Added';
+            }else{
+                $update = new TeacherModel( (int) $_POST['id'], $_POST['first_name'], $_POST['last_name'], $_POST['email']);
+                $pdo->updateTeacher($update);
+
+                $message= 'Teacher Updated';
+
+            }
+
+        }
+        if(!empty($_POST['delete'])){
+            $delete = $pdo->getTeacher((int)$_POST['id']);
+            $pdo->deleteTeacher($delete);
+
+            $message= 'Teacher Deleted';
+        }
+
+        if (isset($_GET)) {
+            switch ($_GET['run'] ?? '') {
+                case 'create':
+                    require 'View/teacherCreate.php';
+                    break;
+                case 'detailed':
+                    $teacher = $pdo->getTeacher((int)$_GET['id']); //placeholder
+                    require 'View/teacherDetail.php';
+                    break;
+                case 'update':
+                    $teacher = $pdo->getTeacher((int)$_GET['id']);
+                    require 'View/teacherEdit.php';
+                    break;
+                default:
+                    $teachers = $pdo->getAllTeaches();
+                    require 'View/teacherOverview.php';
+                    break;
+            }
+        }
+
     }
+
 }
+
