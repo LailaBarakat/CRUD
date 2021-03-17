51ccfbafc052e4b3258a2935e9e1a14faa2081f3
Thomas Pieters <pieters.tg@gmail.com>
2021-03-17 15:39:40 +0100

    Added basic frame for classcontroller

--- Controller/ClassController.php
+++ Controller/ClassController.php
@@ -6,13 +6,48 @@ class ClassController
     //render function with both $_GET and $_POST vars available if it would be needed.
     public function render(array $GET, array $POST)
     {
-        //this is just example code, you can remove the line below
-        $user = new User('John Smith');
+        $pdo = new ClassLoader();
 
-        //you should not echo anything inside your controller - only assign vars here
-        // then the view will actually display them.
+        if(!empty($_POST['name']) && !empty($_POST['teacherID'])){
+            if(empty($_POST['id'])){
+                $new = new ClassModel();
+                //call method to add new class
 
-        //load the view
-        require 'View/homepage.php';
+                $message= 'New Student Added';
+            }else{
+                $update = new ClassModel();
+                //call method to update class
+
+                $message= 'Class Updated';
+
+            }
+
+        }
+        if(!empty($_POST['delete'])){
+            //add method to call class
+            //call method to delete class
+
+            $message= 'Class Deleted';
+        }
+
+        if (isset($_GET)) {
+            switch ($_GET['run'] ?? '') {
+                case 'create':
+                    require 'View/classCreate.php';
+                    break;
+                case 'detailed':
+                    //call a specific class
+                    require 'View/classDetail.php';
+                    break;
+                case 'update':
+                    //call a specific class
+                    require 'View/classEdit.php';
+                    break;
+                default:
+                    //call all classes
+                    require 'View/classOverview.php';
+                    break;
+            }
+        }
     }
 }
