7a1966da716c6bffe572fc8fa437414fc8dc3b22
serkan <s.safran84@gmail.com>
2021-03-18 10:02:52 +0100

    fixing button and form

--- View/studentCreate.php
+++ View/studentCreate.php
@@ -9,42 +9,43 @@ require 'includes/header.php'?>
     <!-- this is the view, try to put only simple if's and loops here.
     Anything complex should be calculated in the model -->
 
-        <h4>Create New Student</h4>
+    <h4 xmlns="http://www.w3.org/1999/html">Create New Student</h4>
     <form method="post" id="create-student">
-        <input type="hidden" name="id" value="">
-        <div>
-        <label for="first_name">First name:</label>
-        <input type="text" name="first_name" id="first_name" required />
+        <div class="form-group container container-fluid">
+            <input type="hidden" name="id" value="" class="form-control">
+            <label for="first_name" class="form-label">First name:</label>
+            <input type="text" name="first_name" id="first_name" required class="form-control" />
+
+
+
+            <label for="last_name" class="form-label">Last name:</label>
+            <input type="text" name="last_name" id="last_name"  required class="form-control" />
+
+
+
+            <label for="email" class="form-label">Email:</label>
+            <input type="text" name="email" id="email" class="form-control" required/>
+
+
+
+            <label for="class" class="form-label">Class:</label>
+            <input type="text" name="class" id="class"  class="form-control" required/>
+            <button type="submit" class="btn btn-primary mt-4">Create Student</button>
         </div>
 
-        <div>
-        <label for="last_name">Last name:</label>
-        <input type="text" name="last_name" id="last_name"  required />
-        </div>
-
-        <div>
-        <label for="email">Email:</label>
-        <input type="text" name="email" id="email"/>
-        </div>
-
-        <div >
-        <label for="class">Class:</label>
-        <input type="text" name="class" id="class" />
-        </div>
-        <input type="submit" value="Crate Student" class="btn btn-primary mt-4 ml-4"/>
     </form>
 
 
-
-    <style>
-        label {
-            cursor: pointer;
-        }
-        label {
-            display: block;
-        }
-        form#create-student {
-            margin-left: 10px;
-        }
-    </style>
+<!---->
+<!--    <style>-->
+<!--        label {-->
+<!--            cursor: pointer;-->
+<!--        }-->
+<!--        label {-->
+<!--            display: block;-->
+<!--        }-->
+<!--        form#create-student {-->
+<!--            margin-left: 10px;-->
+<!--        }-->
+<!--    </style>-->
 <?php require 'includes/footer.php'?>
