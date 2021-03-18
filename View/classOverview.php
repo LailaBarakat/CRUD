699a1fc4b579ef95d839e5a9704ebe6c28d401ea
Thomas Pieters <pieters.tg@gmail.com>
2021-03-18 08:11:33 +0100

    Added functionality by resolving  several naming issues

--- View/classOverview.php
+++ View/classOverview.php
@@ -1,65 +1,67 @@
 <?php
 
-declare(strict_types=1);
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
 require "includes/header.php"
+
 ?>
 
-<h1>Students</h1>
+    <h1>Classes</h1>
 
-<?php if(isset($message)):?>
+<?php if (isset($message)): ?>
     <div class="alert alert-success" role="alert">
-        <?php echo $message?>
+        <?php echo $message ?>
     </div>
-<?php endif;?>
+<?php endif; ?>
 
-<!--students table-->
-<table class="table table-striped table-wide">
-    <thead>
-    <tr>
-        <th width="20%">Location</th>
-        <th width="20%">Name</th>
-        <th width="20%">Teacher ID</th>
-        <td colspan="2" width="20%"></td>
-    </tr>
-    </thead>
-    <tbody>
-    <?php foreach($classes AS $class):?>
+    <!--classes table-->
+    <table class="table table-striped table-wide">
+        <thead>
         <tr>
-            <td><?php echo htmlspecialchars($class->getlocation())?></td>
-            <td><?php echo htmlspecialchars($class->getname())?></td>
-            <td><?php echo htmlspecialchars($class->getteacherid())?></td>
-            <td>
-                <a href="?id=<?php echo $class->getid()?>" class="btn btn-primary">Edit</a>
-            </td>
-            <td>
-                <form method="post">
-                    <input type="hidden" name="id" value="<?php echo $class->getid()?>" />
-                    <input type="submit" name="delete" value="Delete" class="btn btn-danger">
-                </form>
-            </td>
-            <td>
-                <input type="submit" name="Info" value="info" class="btn btn-success">
-            </td>
+            <th width="20%">Location</th>
+            <th width="20%">Name</th>
+            <th width="20%">Teacher ID</th>
+            <td colspan="2" width="20%"></td>
         </tr>
-        </tr>
-    <?php endforeach;?>
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+        <?php foreach ($classes as $class): ?>
+            <tr>
+                <td><?php echo htmlspecialchars($class->getclassname()) ?></td>
+                <td><?php echo htmlspecialchars($class->getclasslocation()) ?></td>
+                <td><?php echo htmlspecialchars($class->getteacherid()) ?></td>
+                <td>
+                    <a href="?page=class&run=update&id=<?php echo $class->getclassid() ?>"
+                       class="btn btn-primary">Edit</a>
+                </td>
+                <td>
+                    <form method="post">
+                        <input type="hidden" name="id" value="<?php echo $class->getclassid() ?>"/>
+                        <input type="submit" name="delete" value="Delete" class="btn btn-danger">
+                    </form>
+                </td>
+                <td>
+                    <a href="?page=class&run=detailed&id=<?php echo $class->getclassid() ?>" class="btn btn-success">Info</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
 
-<style>
-    label {
-        cursor: pointer;
-    }
-    label {
-        display: block;
-    }
-    form#create-user {
-        margin-left: 10px;
-    }
-</style>
+    <div class="col-2 mx-auto">
+        <a href="?page=class&run=create" class="btn btn-primary ">Add New Class</a>
+    </div>
 
-<?php require "includes/footer.php"?>
+    <style>
+        label {
+            cursor: pointer;
+        }
+
+        label {
+            display: block;
+        }
+
+        form#create-user {
+            margin-left: 10px;
+        }
+    </style>
+
+<?php require "includes/footer.php" ?>
