c2c5ba62468e55bafc6a6135d0675c187af88db0
Thomas Pieters <pieters.tg@gmail.com>
2021-03-18 09:47:23 +0100

    Corrected naming in nav-link

--- View/includes/navigation.php
+++ View/includes/navigation.php
@@ -3,13 +3,13 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <a class="nav-link" href="?page=students">Students</a>
+                <a class="nav-link" href="?page=student">Students</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="?page=teachers">Teachers</a>
+                <a class="nav-link" href="?page=teacher">Teachers</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="?page=classes">Classes</a>
+                <a class="nav-link" href="?page=class">Classes</a>
             </li>
         </ul>
 
